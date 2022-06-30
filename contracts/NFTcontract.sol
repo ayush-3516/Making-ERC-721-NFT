bfb1f7e83411ccdf768e50c183deed726940e1b8
ayush-3516 <letsmail.ayushchaudhari@gmail.com>
2022-06-30 22:24:13 +0530

    Update NFTcontract.sol

--- contracts/NFTcontract.sol
+++ contracts/NFTcontract.sol
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-
 contract NFTcontract is ERC721 {
-    constructor(string memory name, string memory symbol)
-        ERC721(name, symbol)
-    {}
+    constructor(string memory name, string memory symbol) ERC721(name, symbol){
+        
+    }
 }
