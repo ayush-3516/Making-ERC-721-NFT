b67e05091d68eac662c6436913e3d1c01aa2ecae
ayush-3516 <letsmail.ayushchaudhari@gmail.com>
2022-07-01 19:49:39 +0530

    Updating Dependencies!

--- contracts/NFTcontract.sol
+++ contracts/NFTcontract.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
+
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-contract NFTcontract is ERC721 {
-    constructor(string memory name, string memory symbol) ERC721(name, symbol){
-        
-    }
-}
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.col";
+import "@openzeppelin/contracts/ERC721/extensions/ERC721URIStorage.sol";
+
+contract NFTcontract is ERC721URIStorage, Ownable {}
