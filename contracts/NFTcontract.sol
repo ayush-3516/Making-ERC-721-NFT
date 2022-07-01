9af8af47b9f52e2829bae5635294a8325ce8db5f
ayush-3516 <letsmail.ayushchaudhari@gmail.com>
2022-07-01 23:21:26 +0530

    Updating Smart Contract!

--- contracts/NFTcontract.sol
+++ contracts/NFTcontract.sol
@@ -1,9 +1,27 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.15;
+
+pragma solidity >=0.5.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.col";
-import "@openzeppelin/contracts/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract NFTcontract is ERC721URIStorage, Ownable {}
+contract NFTcontract is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("Ayush.eth", "AC3") {}
+
+    function mintNFT(address recepient, string memory tokenURI)
+        public
+        onlyOwner
+        returns (uint256)
+    {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _mint(recepient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        return newItemId;
+    }
+}
